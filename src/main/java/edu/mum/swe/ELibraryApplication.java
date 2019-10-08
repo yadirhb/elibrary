dc96af5ab7d65a0d9b29f44c2d1a3ba8b9b88255
Yadir Hernandez <yadirhb@gmail.com>
2019-10-08 15:10:44 -0500

    Added lab project eLibrary

---
+++ src/main/java/edu/mum/swe/ELibraryApplication.java
@@ -0,0 +1,13 @@
+package edu.mum.swe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELibraryApplication.class, args);
+	}
+
+}
